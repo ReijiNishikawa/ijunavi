e68a16d8e1886a8658bb288348be65861909a1e8
1F10230214 <S1F102302136@iniad.org>
2025-09-23 03:28:52 +0900

    signup,loginpe-jinotuila

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from ijunavi import views as ijunavi_views
 
 urlpatterns = [
+        path('login/', ijunavi_views.login_view, name='login'),
+    path('signup/', ijunavi_views.signup_view, name='signup'),
     path('admin/', admin.site.urls),
+    path('', ijunavi_views.top, name='top'),
 ]
+
--- ijunavi/views.py
+++ ijunavi/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import random
+from django.http import HttpResponse
+from django.http import Http404
+from django.utils import timezone
 
 # Create your views here.
+def top(request):
+    return render(request, 'ijunavi/top.html')
+
+def login_view(request):
+    return render(request, 'ijunavi/login.html')
+
+def signup_view(request):
+    return render(request, 'ijunavi/signup.html')
