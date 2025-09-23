5803627a351acf810a94d9be0cc725ef9eece2dd
1f10230260 <s1f102302607@iniad.org>
2025-09-23 19:44:45 +0900

    新規登録だとか

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.exceptions import ValidationError
+from . import forms
 
-# Create your views here.
+
+def signin(request):
+    signin_form = forms.SignInForm(request.POST or None)
+    if signin_form.is_valid():
+        try:
+            signin_form.save()
+            return redirect('accounts:home')
+        except ValidationError as e:
+            signin_form.add_error('password', e)
+    return render(request,'accounts/signin.html', context={
+        'signin_form': signin_form,
+    })
