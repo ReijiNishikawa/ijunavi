520d33aa1e7ec845c9f4968c0c9b5faa72fe1d68
1f10230260 <s1f102302607@iniad.org>
2025-09-23 19:23:54 +0900

    ユーザー識別？用のデータベースつくったよ

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import UserManager
 
 # Create your models here.
+class Users(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    icon = models.FileField(null=True, upload_to='icon/')
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        db_table = 'users'
--- authtest/views.py
+++ authtest/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -9,4 +10,15 @@ def top(request):
     return render(request, 'ijunavi/top.html', {})
 
 def login(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "login.html", {"erroe": "メールまたはパスワードが違います"})
+        
     return render(request, 'login.html', {})
