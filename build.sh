2bf97d106cabf2b5cf418a30978454ca352165d7
1F10230214 <S1F102302136@iniad.org>
2026-01-12 23:48:34 +0900

    deploy debag

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 python -m pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
